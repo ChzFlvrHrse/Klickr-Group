d5989e80ca13b55451af569b55634a32d2ea414b
Nathan Scott <nate@Nates-PC.localdomain>
2022-10-17 17:40:41 -0500

    like button and upload

--- react-app/src/components/ImageDetailsPage/imageDetails.js
+++ react-app/src/components/ImageDetailsPage/imageDetails.js
@@ -1,116 +1,114 @@
-// import { useEffect, useState } from 'react';
-// import { useSelector, useDispatch } from 'react-redux';
-// import { useParams, useHistory, Link } from "react-router-dom";
-// import { getOneImageThunk } from '../../store/image';
-// import "./imageDetails.css"
-
-// function ImageDetails() {
-//     const image = useSelector(state => state.image)
-
-//     const { id } = useParams()
-
-//     const dispatch = useDispatch()
-
-//     useEffect(() => {
-//         dispatch(getOneImageThunk(id))
-//     }, [dispatch])
-
-//     return (
-//         <div id="details-container">
-//             <div id="back-explore">
-//                 <Link to="/" className='i'><i class="fa-solid fa-arrow-left"></i></Link>
-//                 <Link to="/">Back to explore</Link>
-//             </div>
-//             <div>
-//                 <img src={image.previewImageUrl} />
-//             </div>
-//         </div>
-//     )
-// }
-
-// export default ImageDetails;
-import "./imageDetails.css";
-
 import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { useParams } from "react-router-dom";
-
-import { Modal } from "../../context/Modal";
-
-import { getAllUsersThunk } from "../../store/AllUsers";
-import { getImagesThunk, getOneImageThunk } from "../../store/image";
-import {
-  getAllCommentsThunk,
-  getImageCommentsThunk,
-} from "../../store/comments";
-import {
-  getImageLikesThunk,
-  createLikesThunk,
-  deleteLikesThunk,
-} from "../../store/likes";
-
+import { useParams, Link } from "react-router-dom";
+import { getOneImageThunk, getImagesThunk } from '../../store/image';
 import EditImageForm from "../ImagesForms/EditImageForm";
 import DeleteImageForm from "../ImagesForms/DeleteImageForm";
 import CreateCommentForm from "../Comments/CreateCommentForm";
 import EditCommentForm from "../Comments/EditCommentForm";
 import DeleteCommentForm from "../Comments/DeleteCommentForm";
+import { getAllUsersThunk } from "../../store/AllUsers";
+import { getAllCommentsThunk, getImageCommentsThunk } from "../../store/comments";
+import { getImageLikesThunk, createLikesThunk, deleteLikesThunk } from "../../store/likes";
+import { Modal } from "../../context/Modal";
+import "./imageDetails.css"
 
-export function ImageDetails() {
-  const dispatch = useDispatch();
-  const { imageId } = useParams();
-  //   variables
-  let allImagesArray;
-  let allImagesFiltered;
-  // useEffects
-  useEffect(() => {
-    dispatch(getImagesThunk());
-  }, [dispatch]);
+function ImageDetails() {
 
-  useEffect(() => {
-    dispatch(getAllUsersThunk());
-  }, [dispatch]);
+    const dispatch = useDispatch();
+    const { id } = useParams();
+    //   variables
+    let allImagesArray;
+    let allImagesFiltered;
+    // useEffects
+    useEffect(() => {
+        dispatch(getImagesThunk());
+    }, [dispatch]);
 
-  useEffect(() => {
-    dispatch(getImageLikesThunk(imageId))
- }, [dispatch])
+    useEffect(() => {
+        dispatch(getAllUsersThunk());
+    }, [dispatch]);
 
- useEffect(() => {
-  dispatch(getAllCommentsThunk());
-}, [dispatch]);
+    useEffect(() => {
+        dispatch(getImageLikesThunk(id))
+    }, [dispatch])
 
-  // redux states
-  const images = useSelector((state) => state.image);
-  const allusers = useSelector((state) => state.allUsers);
-  const user = useSelector((state) => state.session.user);
-  // filters
-  allImagesArray = Object.values(images);
-  const allUsersArray = Object.values(allusers);
+    useEffect(() => {
+        dispatch(getAllCommentsThunk());
+    }, [dispatch]);
 
-  allImagesFiltered = allImagesArray.filter((filteredImages, index) => filteredImages.id == imageId)
-// if image does not exist
-  if (!allImagesFiltered.length) {
+
+    // redux states
+    const images = useSelector((state) => state.image);
+    const allusers = useSelector((state) => state.allUsers);
+    const user = useSelector((state) => state.session.user);
+    const likes = useSelector(state => state.likes);
+
+    let likesArray = Object.values(likes);
+    let filteredLikes;
+
+    filteredLikes = likesArray.filter((filteredLikes, index) => filteredLikes.userId == user.id)
+    const userLikeId = filteredLikes[0]
+    // console.log(userLikeId)
+    // toggle likes on and off (post and delete)
+    const toggleLikes = (e) => {
+        e.preventDefault();
+        if (!filteredLikes.length) {
+            dispatch(createLikesThunk(id))
+        }
+        else {
+            dispatch(deleteLikesThunk(userLikeId.id))
+        }
+
+    };
+
+    // filters
+    allImagesArray = Object.values(images);
+    const allUsersArray = Object.values(allusers);
+
+    allImagesFiltered = allImagesArray.filter((filteredImages, index) => filteredImages.id == id)
+    // console.log(allImagesFiltered[0])
+
+    let imageOwner = allUsersArray.filter(user => user.id == allImagesFiltered[0].userId)
+    let owner = imageOwner[0]
+
+    // if image does not exist
+    if (!allImagesFiltered.length) {
+        return (
+            <>
+                <div>
+                    Sorry this image does not exist!
+                </div>
+            </>
+        )
+    }
+
+
+
+    //   else return everything
     return (
         <>
-        <div>
-            Sorry this image does not exist!
-        </div>
+            <div id="details-image">
+                <div id="back-explore">
+                    <Link to="/" className='i'><i class="fa-solid fa-arrow-left"></i></Link>
+                    <Link to="/">Back to explore</Link>
+                </div>
+                <div id="user-image">
+                    <img src={allImagesFiltered[0].previewImageUrl} />
+                </div>
+                <div id="star-like">
+                    {filteredLikes.length ? <i class="fa-solid fa-star" onClick={toggleLikes}></i> : <i class="fa-regular fa-star" onClick={toggleLikes}></i>}
+                    <Link to="/upload"><i class="fa-solid fa-download"></i></Link>
+                </div>
+            </div>
+            <div id='image-info'>
+                <div>
+                    {owner && (<h1>{owner.username}</h1>)}
+                </div>
+            </div>
         </>
-    )
-  }
-
-
-
-  // else return everything
-  return (
-    <>
-      <div className="ImageDetailsEntireContainerOuter">
-        <div className="ImageDetailsUpperHalfContainer">
-          <h1>Let's Get Started</h1>
-        </div>
-        <div className="ImageDetailsLowerHalfContainer">
-          <h1>Let's Finish This</h1>
-        </div>
-      </div>
-    </>
-  );
+    );
 }
+
+export default ImageDetails;
+
