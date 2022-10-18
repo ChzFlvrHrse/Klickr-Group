fd53d78b0850c0385837988120921ef0fbc49b9b
Keanenb <keanenbafekr@me.com>
2022-10-18 04:38:51 -0400

    Update user.py

--- app/models/user.py
+++ app/models/user.py
@@ -38,5 +38,6 @@ class User(db.Model, UserMixin):
             'first_name': self.first_name,
             'last_name': self.last_name,
             'username': self.username,
-            'email': self.email
+            'email': self.email,
+            'previewImageUrl': self.previewImageUrl
         }
