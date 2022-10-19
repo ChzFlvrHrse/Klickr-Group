d9e64b9d45cbc921312cdd43a5ae13da436be69c
Justinguu <justinguuu@gmail.com>
2022-10-18 23:10:40 -0400

    revamped signup

--- react-app/src/components/auth/SignUpForm.js
+++ react-app/src/components/auth/SignUpForm.js
@@ -1,33 +1,55 @@
-import React, { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux'
-import { Redirect } from 'react-router-dom';
-import { signUp } from '../../store/session';
-import klickrLogo from '../../icons/Klickr-logo.png'
-import "./SignUpForm.css"
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Redirect } from "react-router-dom";
+import { signUp } from "../../store/session";
+import klickrLogo from "../../icons/Klickr-logo.png";
+import * as sessionActions from "../../store/session";
+import "./SignUpForm.css";
 
 const SignUpForm = () => {
   const [errors, setErrors] = useState([]);
-  const [username, setUsername] = useState('');
-  const [first_name, setFirstName] = useState('');
-  const [last_name, setLastName] = useState('');
-  const [previewImageUrl, setPreviewImageUrl] = useState('');
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [repeatPassword, setRepeatPassword] = useState('');
-  const user = useSelector(state => state.session.user);
+  const [username, setUsername] = useState("");
+  const [first_name, setFirstName] = useState("");
+  const [last_name, setLastName] = useState("");
+  const [previewImageUrl, setPreviewImageUrl] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [repeatPassword, setRepeatPassword] = useState("");
+  const user = useSelector((state) => state.session.user);
+  
   const dispatch = useDispatch();
 
-  const onSignUp = async (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault();
-    if (previewImageUrl == null || !previewImageUrl.endsWith('.jpg') || !previewImageUrl.endsWith('.png') || !previewImageUrl.endsWith('.jpeg')) {
-      setPreviewImageUrl('https://creazilla-store.fra1.digitaloceanspaces.com/emojis/55737/grinning-face-with-big-eyes-emoji-clipart-xl.png')
+    if (
+      previewImageUrl == null ||
+      !previewImageUrl.endsWith(".jpg") ||
+      !previewImageUrl.endsWith(".png") ||
+      !previewImageUrl.endsWith(".jpeg")
+    ) {
+      setPreviewImageUrl(
+        "https://creazilla-store.fra1.digitaloceanspaces.com/emojis/55737/grinning-face-with-big-eyes-emoji-clipart-xl.png"
+      );
     }
     if (password === repeatPassword) {
-      const data = await dispatch(signUp(username, email, first_name, last_name, previewImageUrl, password));
-      if (data) {
-        setErrors(data)
-      }
+      setErrors([]);
+      return dispatch(sessionActions.signUp({
+          username,
+          email,
+          first_name,
+          last_name,
+          previewImageUrl,
+          password,
+        })
+      )
+        .catch(async (res) => {
+          const data = await res.json();
+          if (data && data.errors) setErrors(data.errors);
+        });
     }
+    return setErrors([
+      "Password fields needs to be the same",
+    ]);
   };
 
   const updateUsername = (e) => {
@@ -58,99 +80,106 @@ const SignUpForm = () => {
   };
 
   if (user) {
-    return <Redirect to='/' />;
+    return <Redirect to="/" />;
   }
 
   return (
     <>
-      <div>
-        {errors.map((error, ind) => (
-          <div key={ind}>{error}</div>
-        ))}
-      </div>
-      <div className='login-container'>
-        <div className='inner-login'>
-          <form className='login-form' onSubmit={onSignUp}>
+
+      <div className="signup-container">
+        <div className="inner-signup">
+          <form className="signup-form" onSubmit={handleSubmit}>
             <div id="circles-container">
-              <img src={klickrLogo} alt='' id='circles'></img>
+              <img src={klickrLogo} alt="" id="circles"></img>
             </div>
-            <h4 id='to-klickr'>
-              Sign up for Klickr
-            </h4>
-            <div className='label-input'>
+            <h4 id="to-klickr">Sign up for Klickr</h4>
+            <div className="signup-form-errors">
+                {errors.length > 0 && (
+                  <div className="signup-errors-wrapper" >
+                    {errors.map((error, idx) => (
+                      <div  key={idx}>{error}</div>
+                    ))}
+                  </div>
+                )}
+              </div>
+            <div className="label-input">
               <label id="labelInputSignUpName">User Name</label>
               <input
-                type='text'
-                name='username'
+                type="text"
+                name="username"
                 onChange={updateUsername}
                 value={username}
+                required
               ></input>
             </div>
-            <div className='label-input'>
-              <label id='labelInputSignUpName'>First Name</label>
+            <div className="label-input">
+              <label id="labelInputSignUpName">First Name</label>
               <input
-                type='text'
-                name='first_name'
+                type="text"
+                name="first_name"
                 onChange={updateFirstName}
                 value={first_name}
+                required
               ></input>
-            </div >
-            <div className='label-input'>
-              <label id='labelInputSignUpName'>Last Name</label>
+            </div>
+            <div className="label-input">
+              <label id="labelInputSignUpName">Last Name</label>
               <input
-                type='text'
-                name='last_name'
+                type="text"
+                name="last_name"
                 onChange={updateLastName}
                 value={last_name}
+                required
               ></input>
-            </div >
-            <div className='label-input'>
+            </div>
+            <div className="label-input">
               <label>Email address</label>
               <input
-                type='text'
-                name='email'
+                type="text"
+                name="email"
                 onChange={updateEmail}
                 value={email}
+                required
               ></input>
-            </div >
-            <div className='label-input'>
+            </div>
+            <div className="label-input">
               <label>Profile Picture</label>
               <input
-                type='text'
-                name='previewImageUrl'
+                type="text"
+                name="previewImageUrl"
                 onChange={updatePreviewImageUrl}
                 value={previewImageUrl}
                 required={false}
               ></input>
-            </div >
-            <div className='label-input'>
-              <label id='password'>Password</label>
+            </div>
+            <div className="label-input">
+              <label id="password">Password</label>
               <input
-                type='password'
-                name='password'
+                type="password"
+                name="password"
                 onChange={updatePassword}
                 value={password}
+                required
               ></input>
             </div>
-            <div className='label-input'>
-              <label id='repeat'>Repeat Password</label>
+            <div className="label-input">
+              <label id="repeat">Repeat Password</label>
               <input
-                type='password'
-                name='repeat_password'
+                type="password"
+                name="repeat_password"
                 onChange={updateRepeatPassword}
                 value={repeatPassword}
-                required={true}
+                required
               ></input>
-            </div>
-            <div className='button'>
-              <button type='submit'>Sign Up</button>
+                </div>
+              
+            <div className="button">
+              <button type="submit">Sign Up</button>
             </div>
           </form>
         </div>
       </div>
-
     </>
-
   );
 };
 
